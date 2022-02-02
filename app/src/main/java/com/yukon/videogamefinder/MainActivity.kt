cd8fa68c15ae9591c787770dd332254fe2d255bc
Yukon2077 <yuvankumar2077@gmail.com>
2022-02-02 23:02:16 +0530

    Second commit

--- app/src/main/java/com/yukon/videogamefinder/MainActivity.kt
+++ app/src/main/java/com/yukon/videogamefinder/MainActivity.kt
@@ -3,6 +3,8 @@ package com.yukon.videogamefinder
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import com.google.android.material.button.MaterialButton
 
@@ -19,6 +21,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(view : View?) {
         val intent = Intent(this, GameListActivity::class.java)
         startActivity(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings -> { startActivity(Intent(this, GameListActivity::class.java))
+            return true }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
